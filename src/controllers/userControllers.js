8cc9661a9fea491a2ee1f8600e0bd85f0e60d223
Kewyn Silva Gonçalves <kewyngoncalves612@gmail.com>
2026-02-09 16:47:04 -0300

    teste to push

--- src/controllers/userControllers.js
+++ src/controllers/userControllers.js
@@ -41,7 +41,7 @@ const getUserById = async (req, res) => {
         const user = await User.findById(req.params.id);
         res.status(200).json(user);
     } catch (error) {
-        res.status(404).json({ message: "User not found" });
+        res.status(404).json({ message: "User not found" })
     }
 }
 
